02b3dc56b1bee42418e420d7d331cf4177637ce9
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-15 22:25:38 +0530

    hooks 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-
 import './App.css';
-import PostForm from './HTTP/PostForm';
+import HookCounter from './Hooks/Component/HookCounter';
 
 function App() {
   return (
     <div className="App">
-    <PostForm/>
- 
+      <HookCounter/>
     </div>
   );
 }
 
 export default App;
+//  Use hooks at top level:don't use it in loops, nested funtions or condirtions
+//  Onlycall hooks from react Function
 
+
+//functional hooks
+//     <HookCounter/>
+
+//class hooks
+//  <ClassCounter/>
 
 // Get request:
 // <PostList/>
+// Post request:
+// <PostForm/>
