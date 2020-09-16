f25868187907da3f7b87777fc2b398a83dd1e8d2
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-16 12:05:35 +0530

    useState with object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HookCounterTwo from './Hooks/useState/HookCounterTwo';
+import HookCounterThree from './Hooks/useState/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo/>
+      <HookCounterThree/>
     </div>
   );
 }
