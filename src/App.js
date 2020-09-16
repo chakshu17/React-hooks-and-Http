ed0a3f36983da0a9cf5df11cfb39e0dd32137695
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-16 12:12:56 +0530

    useState with Array

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
-import HookCounterThree from './Hooks/useState/HookCounterThree';
+import HookCounterFour from './Hooks/useState/HookCounterFour';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree/>
+     <HookCounterFour/>
     </div>
   );
 }
 
 export default App;
+//useState with array:       <HookCounterFour/>
+
+
+// <HookCounterThree/>
+// useState with object: ...name, like this,Spread operator, 
+// this means that all the object , data is passed but only one is edited which is mentioned after it.
+// acc to condition 
+
 //UseState
 //  <HookCounterTwo/>
+
 // when you have to update the value regarding the previous state , always
 // use function like HookCOunterTwo: increment 5
+
+
 
 //  Use hooks at top level:don't use it in loops, nested funtions or condirtions
 //  Onlycall hooks from react Function
